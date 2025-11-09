ef9593d478b60f0c2258f5351c40bf67eb5c8e5b
Manya <manya111203@gmail.com>
2025-11-09 14:58:00 +0530

    update: minor changes

--- src/views/management/orders/Orders.js
+++ src/views/management/orders/Orders.js
@@ -63,7 +63,11 @@ const ORDER_STATUS = {
 // Style Constants for Consistency
 const textStyle = { fontSize: '14px', fontWeight: '500', color: '#2c3e50', letterSpacing: '-0.01em' };
 const subTextStyle = { fontSize: '13px', color: 'black', fontWeight: '400', maxWidth: '150px', display: 'block', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' };
-const subHeaderStyle = { fontSize: '14px', fontWeight: '600', textTransform: 'uppercase', color: 'black', letterSpacing: '0.5px', maxWidth: '30px', textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' };
+const subHeaderStyle = { fontSize: '14px', fontWeight: '600', textTransform: 'uppercase', color: 'black', letterSpacing: '0.5px', 
+    // maxWidth: '30px',
+    textOverflow: 'ellipsis',
+    // whiteSpace: 'nowrap',
+    overflow: 'hidden' };
 
 // function to format date
 const formatDate = (dateString) => {
