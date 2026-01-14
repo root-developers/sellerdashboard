c0136a6a25fa15a3920d6fb84bd2edce63d4b077
Manya <manya111203@gmail.com>
2026-01-14 20:34:35 +0530

    update: prod url

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,5 @@
 const mainDomain = "https://kuality.co.in/";
-const baseUrl = "https://multivendorplatform.onrender.com/";
+const baseUrl = "https://kuality-server.onrender.com";
 
 const GetAccessToken = () => {
   const user = localStorage.getItem("user")
